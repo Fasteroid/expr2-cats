9d6ee3e15cc3581a2cd956765adefa38bcfbfb5d
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2025-07-09 20:55:22 -0400

    csv time

--- src/index.ts
+++ src/index.ts
@@ -9,49 +9,31 @@ const Jimp = createJimp({
   plugins: defaultPlugins,
 });
 
-type DigiImage = {
-	width: number;
-	height: number;
-
-	/** BGR format, 2d array */
-	rows: string[]
-}
-
 const B = 1
 const G = 256
 const R = 256 * 256
 
-
-function toDigi(bmp: Bitmap): DigiImage {
-
-	let rows:   string[] = [];
-	let buffer: number[] = []
+function toDigi(bmp: Bitmap): string {
+	let pixels: number[] = [];
 
 	for (let i = 0; i < bmp.data.length; i += 4) {
-		if( buffer.length >= bmp.width ){ // cuz JSONToTable sucks!
-			rows.push( JSON.stringify(buffer) ); buffer = [] 
-		}
-		buffer.push( 
+		pixels.push( 
 			bmp.data[i] * R + 
 			bmp.data[i + 1] * G + 
 			bmp.data[i + 2] * B 
 		)
 	}
-
-	rows.push( JSON.stringify(buffer) );
 	
-	return {
-		width: bmp.width,
-		height: bmp.height,
-		rows
-	}
+	return [bmp.width, bmp.height, ...pixels].join(",")
 }
+
+// w,h,pixels...
 
 export default {
 	async fetch(): Promise<Response> {
 		const image = await Jimp.read("https://cataas.com/cat");
 		image.scaleToFit({w: 256, h: 256})
 
-		return new Response( JSON.stringify( toDigi(image.bitmap) ) );
+		return new Response( toDigi(image.bitmap) );
 	},
 } satisfies ExportedHandler<Env>;
